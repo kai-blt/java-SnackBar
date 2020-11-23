393677f8edb97062e1aeeac5e73d876ba8180148
kai-blt <blaitong@gmail.com>
2020-11-23 13:01:10 -0800

    completed Snack class

--- src/snackbar/Snack.java
+++ src/snackbar/Snack.java
@@ -2,12 +2,74 @@ package snackbar;
 
 public class Snack {
     //fields
-
-    //default constructor
+    private static int maxId = 0; //class field (shared by all Snack class instances)
+    private int id;
+    private String name;
+    private int quantity;
+    private double cost;
+    private int vendingMachineId;
 
     //constructor initialized state
+    Snack(String name, int quantity, double cost, int vendingMachineId){
+        //increment maxId on Snack instantiation
+        maxId++;
+        //Assign this instance of Snack the current maxId
+        id = maxId;
+        this.name = name;
+        this.quantity = quantity;
+        this.cost = cost;
+        this.vendingMachineId = vendingMachineId;
+    }
 
-    //getters
+    //get Id
+    public int getId() {
+        return id;
+    }
 
-    //setters
+    //get & set Name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //get & set Cost
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    //get & set Vending Machine Id
+    public int getVendingMachineId() {
+        return vendingMachineId;
+    }
+
+    public void setVendingMachineId(int vendingMachineId) {
+        this.vendingMachineId = vendingMachineId;
+    }
+
+    //get quantity
+    public int getQuantity() {
+        return quantity;
+    }
+
+    //add to quantity
+    public void addQuantity(int quantity) {
+        this.quantity += quantity;
+    }
+
+    //buy quantity
+    public void buyQuantity(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    //get total cost
+    public double getTotal(int quantity) {
+        return this.cost *= quantity;
+    }
 }
